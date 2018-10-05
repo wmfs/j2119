f19964435a9b5a75a22f315eb02452ec043631eb
Jez Higgins <jez@jezuk.co.uk>
2018-10-06 00:20:48 +0100

    test: Validator tests now compile and fail

--- test/validator_test.js
+++ test/validator_test.js
@@ -4,54 +4,51 @@ const chai = require('chai')
 chai.use(require('dirty-chai'))
 const expect = chai.expect
 
-/*
-GOOD = '{ ' +
-       ' "StartAt": "x", ' +
-       ' "States": {' +
-       '  "x": {' +
-       '    "Type": "Pass",' +
-       '    "End": true ' +
-       '  }' +
-       ' } ' +
-       '}'
+const validator = require('../lib')
 
-SCHEMA = File.dirname(__FILE__) + '/../data/AWL.j2119'
-*/
+const GOOD = '{ ' +
+  ' "StartAt": "x", ' +
+  ' "States": {' +
+  '  "x": {' +
+  '    "Type": "Pass",' +
+  '    "End": true ' +
+  '  }' +
+  ' } ' +
+  '}'
+
+const SCHEMA = 'data/AWL.j2119'
 
 describe('J2119 Validator', () => {
-  it('do this', () => fail())
-  /*
-    it 'should accept parsed JSON' do
-      v = J2119::Validator.new SCHEMA
-      j = JSON.parse GOOD
-      p = v.validate j
-      expect(p.empty?).to be true
-    end
+  it('should accept parsed JSON', () => {
+    const v = validator(SCHEMA)
+    const j = JSON.parse(GOOD)
+    const p = v.validate(j)
+    expect(p.length).to.eql(0)
+  })
 
-    it 'should accept JSON text' do
-      v = J2119::Validator.new SCHEMA
-      p = v.validate GOOD
-      expect(p.empty?).to be true
-    end
+  it('should accept JSON text', () => {
+    const v = validator(SCHEMA)
+    const p = v.validate(GOOD)
+    expect(p.length).to.eql(0)
+  })
 
-    it 'should read a JSON file' do
-      v = J2119::Validator.new SCHEMA
-      fn = "/tmp/#{$$}.tjf"
-      f = File.open(fn, "w")
-      f.write GOOD
-      f.close
+  xit('should read a JSON file', () => {
+    /*
+        v = J2119::Validator.new SCHEMA
+        fn = "/tmp/#{$$}.tjf"
+        f = File.open(fn, "w")
+        f.write GOOD
+        f.close
 
-      p = v.validate fn
-      File.delete fn
-      expect(p.empty?).to be true
-    end
-
-    it 'should produce some sort of sane message with bad JSON' do
-      v = J2119::Validator.new SCHEMA
-      p = v.validate GOOD + 'x'
-      expect(p.size).to eq(1)
-    end
-
-  end
+        p = v.validate fn
+        File.delete fn
+        expect(p.empty?).to be true
   */
+  })
+
+  it('should produce some sort of sane message with bad JSON', () => {
+    const v = validator(SCHEMA)
+    const p = v.validate(GOOD + 'x')
+    expect(p.length).to.eql(1)
+  })
 })
