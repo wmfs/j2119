35b434fdbd2eb376aa9392c1ce9b1a862f2bf573
Jez Higgins <jez@jezuk.co.uk>
2018-10-05 12:19:17 +0100

    refactor: Rework conditional exports to align it with constraints

--- test/conditional_test.js
+++ test/conditional_test.js
@@ -4,18 +4,18 @@ const chai = require('chai')
 chai.use(require('dirty-chai'))
 const expect = chai.expect
 
-const roleNotPresentCondition = require('../lib/j2119/conditional')
+const condition = require('../lib/j2119/conditional')
 
 describe('J2119 RoleNotPresent Condition', () => {
   it('should fail on an excluded role', () => {
-    const cut = roleNotPresentCondition(['foo', 'bar'])
+    const cut = condition.roleNotPresent(['foo', 'bar'])
     const json = { 'bar': 1 }
 
     expect(cut.constraintApplies(json, ['foo'])).to.be.false()
   })
 
   it('should succeed on a non-excluded role', () => {
-    const cut = roleNotPresentCondition(['foo', 'bar'])
+    const cut = condition.roleNotPresent(['foo', 'bar'])
     const json = { 'bar': 1 }
     expect(cut.constraintApplies(json, ['baz'])).to.be.true()
   })
