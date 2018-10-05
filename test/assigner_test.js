e3d6919bc96200c08107da38bbcf97a37295d5ba
Jez Higgins <jez@jezuk.co.uk>
2018-10-05 12:19:33 +0100

    feat: Start work on Assigner

---
+++ test/assigner_test.js
@@ -0,0 +1,201 @@
+/* eslint-env mocha */
+
+const chai = require('chai')
+chai.use(require('dirty-chai'))
+const expect = chai.expect
+
+const roleConstraints = require('../lib/j2119/role_constraints')
+const allowedFields = require('../lib/j2119/allowed_fields')
+const lineMatcher = require('../lib/j2119/line_matcher')
+const roleFinder = require('../lib/j2119/role_finder')
+const assigner = require('../lib/j2119/assigner')
+
+describe('J2119 Assigner', () => {
+
+  it('should attach a condition to a constraint', () => {
+    const assertion = {
+      'role': 'R',
+      'modal': 'MUST',
+      'field_name': 'foo',
+      'excluded': 'an A, a B, or a C'
+    }
+    const roles = [ 'A', 'B', 'C' ]
+    const constraints = roleConstraints()
+    const rf = roleFinder()
+    const matcher = lineMatcher('x')
+    roles.forEach(role => matcher.addRole(role))
+    const permittedFields = allowedFields()
+
+    const cut = assigner(constraints, rf, matcher, permittedFields)
+    cut.assignConstraints(assertion)
+
+    const retrieved = constraints.getConstraints('R')
+    const c = retrieved[0]
+    const json = { 'a': 1 }
+    roles.forEach(role =>
+      expect(c.applies(json, [ role ])).to.be.false()
+    )
+    expect(c.applies(json, [ 'foo' ])).to.be.true()
+  })
+
+  it('should handle a "non-zero ... less than" constraint properly', () => {
+    const assertion = {
+      'role': 'R',
+      'modal': 'MAY',
+      'type': 'nonnegative-integer',
+      'field_name': 'MaxAttempts',
+      'relation': 'less than',
+      'target': 99999999
+    }
+    const constraints = roleConstraints()
+    const rf = roleFinder()
+    const matcher = lineMatcher('x')
+    const permittedFields = allowedFields()
+    const cut = assigner(constraints, rf, matcher, permittedFields)
+    cut.assignConstraints(assertion)
+    const retrieved = constraints.getConstraints('R')
+    expect(retrieved[0].toString()).to.eql('trousers')
+  })
+
+  it('should assign an only_one_of constraint properly', () => {
+    const assertion = {
+      'role': 'R',
+      'field_list': '"foo", "bar", and "baz"'
+    }
+    const constraints = roleConstraints()
+    const rf = roleFinder()
+    const matcher = lineMatcher('x')
+    const permittedFields = allowedFields()
+    const cut = assigner(constraints, rf, matcher, permittedFields)
+    cut.assignOnlyOneOf(assertion)
+    const retrieved = constraints.getConstraints('R')
+    expect(retrieved[0].toString()).to.eql('trousers')
+  })
+
+  it('should add a HasFieldConstraint if there\'s a MUST', () => {
+    const assertion = {
+      'role': 'R',
+      'modal': 'MUST',
+      'field_name': 'foo'
+    }
+    const constraints = roleConstraints()
+    const rf = roleFinder()
+    const matcher = lineMatcher('x')
+    const permittedFields = allowedFields()
+    const cut = assigner(constraints, rf, matcher, permittedFields)
+    cut.assignConstraints(assertion)
+    const retrieved = constraints.getConstraints('R')
+    expect(retrieved[0].toString()).to.eql('trousers')
+  })
+
+  it('should add a DoesNotHaveFieldConstraint if there\'s a MUST NOT', () => {
+    const assertion = { 'role': 'R', 'modal': 'MUST NOT', 'field_name': 'foo' }
+    const constraints = roleConstraints()
+    const rf = roleFinder()
+    const matcher = lineMatcher('x')
+    const permittedFields = allowedFields()
+    const cut = assigner(constraints, rf, matcher, permittedFields)
+    cut.assignConstraints(assertion)
+    const retrieved = constraints.getConstraints('R')
+    expect(retrieved[0].toString()).to.eql('trousers')
+  })
+
+  xit('should manage a complex type constraint ', () => {
+    const assertion = { 'role': 'R',
+                  'modal': 'MUST',
+                  'field_name': 'foo',
+                  'type': 'nonnegative-float'
+                }
+    const constraints = roleConstraints()
+    const rf = roleFinder()
+    const matcher = lineMatcher('x')
+    const permittedFields = allowedFields()
+    const cut = assigner(constraints, rf, matcher, permittedFields)
+    cut.assignConstraints(assertion)
+    const retrieved = constraints.getConstraints('R')
+    // c = retrieved.select {|x| x.is_a?(J2119::HasFieldConstraint)}
+    expect(c.length).to.eql(1)
+    // c = retrieved.select {|x| x.is_a?(J2119::FieldTypeConstraint)}
+    expect(c.length).to.eql(1)
+    // c = retrieved.select {|x| x.is_a?(J2119::FieldValueConstraint)}
+    expect(c.length).to.eql(1)
+  })
+
+  xit('should record a relational constraint ', () => {
+    const assertion = { 'role': 'R',
+                  'modal': 'MUST',
+                  'field_name': 'foo',
+                  'type': 'nonnegative-float',
+                  'relation': 'less than'
+                }
+    const constraints = roleConstraints()
+    const rf = roleFinder()
+    const matcher = lineMatcher('x')
+    const permittedFields = allowedFields()
+    const cut = assigner(constraints, rf, matcher, permittedFields)
+    cut.assignConstraints(assertion)
+    const retrieved = constraints.getConstraints('R')
+    // c = retrieved.select {|x| x.is_a?(J2119::HasFieldConstraint)}
+    expect(c.length).to.eql(1)
+    // c = retrieved.select {|x| x.is_a?(J2119::FieldTypeConstraint)}
+    expect(c.length).to.eql(1)
+    // c = retrieved.select {|x| x.is_a?(J2119::FieldValueConstraint)}
+    expect(c.length).to.eql(2)
+  })
+
+  it('should record an is_a role', () => {
+    const assertion = {
+      'role': 'R',
+      'newrole': 'S'
+    }
+    const rf = roleFinder()
+    const constraints = roleConstraints()
+    const matcher = lineMatcher('x')
+    const permittedFields = allowedFields()
+    const cut = assigner(constraints, rf, matcher, permittedFields)
+    cut.assignRoles(assertion)
+    const json = { 'a': 3 }
+    const roles = [ 'R' ]
+    rf.findMoreRoles(json, roles)
+    expect(roles).to.eql([ 'R', 'S' ])
+  })
+
+  it('should correctly assign a field value role', () => {
+    const assertion = {
+      'role': 'R',
+      'fieldtomatch': 'f1',
+      'valtomatch': 33,
+      'newrole': 'S',
+      'val_match_present': true
+    }
+    const constraints = roleConstraints()
+    const rf = roleFinder()
+    const matcher = lineMatcher('R')
+    const permittedFields = allowedFields()
+    const cut = assigner(constraints, rf, matcher, permittedFields)
+    cut.assignRoles(assertion)
+    const json = { 'f1': 33 }
+    const roles = [ 'R' ]
+    rf.findMoreRoles(json, roles)
+    expect(roles).to.eql(['R', 'S'])
+  })
+
+  it('should process a child role in an assertion', () => {
+    const assertion = {
+      'role': 'R',
+      'modal': 'MUST',
+      'field_name': 'a',
+      'child_type': 'field',
+      'child_role': 'bar'
+    }
+    const constraints = roleConstraints()
+    const rf = roleFinder()
+    const matcher = lineMatcher('x')
+    const permittedFields = allowedFields()
+    const cut = assigner(constraints, rf, matcher, permittedFields)
+    cut.assignConstraints(assertion)
+    const roles = [ 'R' ]
+    const fieldRoles = rf.findGrandchildRoles(roles, 'a')
+    expect(fieldRoles).to.eql([ 'bar' ])
+  })
+})
